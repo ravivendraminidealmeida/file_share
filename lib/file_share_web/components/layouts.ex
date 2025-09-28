87b1dc2fcb7c08fb95f21fd350d3060cc3d55baa
RaviVendraminideAlmeida <ravi.vendramini@aluno.ifsp.edu.br>
2025-09-27 23:46:39 -0300

    layout max width change

--- lib/file_share_web/components/layouts.ex
+++ lib/file_share_web/components/layouts.ex
@@ -36,7 +36,7 @@ defmodule FileShareWeb.Layouts do
   def app(assigns) do
     ~H"""
     <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-6xl space-y-4">
+      <div class="mx-auto max-w-8xl space-y-4">
         {render_slot(@inner_block)}
       </div>
     </main>
