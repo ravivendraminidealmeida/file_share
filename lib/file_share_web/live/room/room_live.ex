c8bd8f4b771b36607d81a96e026b76ef4c5d78e1
RaviVendraminideAlmeida <ravivendramini@gmail.com>
2025-09-30 16:05:20 -0300

    Refactor media upload handling and improve layout for better user experience

--- lib/file_share_web/live/room/room_live.ex
+++ lib/file_share_web/live/room/room_live.ex
@@ -1,36 +1,37 @@
 defmodule FileShareWeb.RoomLive do
   use FileShareWeb, :live_view
 
-  def mount(_params, _session, socket) do
+  alias FileShare.MediaRoom
+
+  defp error_to_string(:too_large), do: "Too large"
+  defp error_to_string(:too_many_files), do: "You have selected too many files"
+  defp error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
+
+  def mount(params, _session, socket) do
+    media_room = MediaRoom.get_room!(params["id"])
+
     {:ok,
      socket
-     |> assign(:uploaded_files, [])
-     # This is a mockup and will be replaced before going into prod
-     # or maybe not I don't even know if this is going to prod
-     |> assign(:tracks, ["lorem", "ipsum", "dolor", "sit", "amet", "dolro", "dsajkdakj", "kjhsakjdkjsak"])
-     |> allow_upload(:media, accept: ~w(.jpg .jpeg .mp4))}
+     |> assign(:room, media_room)
+     |> allow_upload(:media, accept: ~w(.jpg .jpeg .mp4 .wav), max_entries: 3)}
   end
 
-  def handle_event("validate", _unsigned_params, socket) do
+  def handle_event("validate_upload", _unsigned_params, socket) do
     {:noreply, socket}
   end
 
-  def handle_event("upload-file", _unsigned_params, socket) do
-    new_entry =
-      consume_uploaded_entries(
-        socket,
-        :media,
-        fn %{path: path}, _entry ->
-          dest =
-            Path.join([:code.priv_dir(:file_share), "static", "uploads", Path.basename(path)])
+  def handle_event("upload_file", _unsigned_params, socket) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :media, fn %{path: path}, _entry ->
+        dest =
+          Path.join(Application.app_dir(:file_share, "priv/static/uploads"), Path.basename(path))
 
-          File.cp!(path, dest)
-          {:ok, ~p"/uploads/{Path.basename(dest)}"}
-        end
-      )
+        File.cp!(path, dest)
+        {:ok, ~p"/uploads/#{Path.basename(dest)}"}
+      end)
 
-    {:noreply,
-     socket
-     |> update(:uploaded_files, &(&1 ++ new_entry))}
+    dbg(uploaded_files)
+
+    {:noreply, socket}
   end
 end
