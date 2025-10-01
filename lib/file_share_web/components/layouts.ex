819f42e20f2e181e25b592c4fedc77a1de8096c3
RaviVendraminideAlmeida <ravivendramini@gmail.com>
2025-10-01 17:38:53 -0300

    layout improvements

--- lib/file_share_web/components/layouts.ex
+++ lib/file_share_web/components/layouts.ex
@@ -35,7 +35,7 @@ defmodule FileShareWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
+    <main class="px-4 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-8xl space-y-4">
         {render_slot(@inner_block)}
       </div>
